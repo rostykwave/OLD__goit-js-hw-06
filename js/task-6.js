e37a25d9d8cc266e2176871ea65a6f411f09f378
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-14 22:24:54 +0300

    7

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,11 @@
 import users from "./users.js";
+
+const getUsersWithAge = (users, min, max) => {
+  // твій код
+    return users.filter(user => user.age >= min && user.age <= max);
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [об'єкт Ross Vazquez, об'єкт Elma Head, об'єкт Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]
--- js/task-7.js
+++ js/task-7.js
@@ -1,1 +1,8 @@
 import users from "./users.js";
+
+const calculateTotalBalance = users => {
+  // твой код
+    return users.map(user => user.balance).reduce((acc, number) => { return acc += number }, 0);
+};
+
+console.log(calculateTotalBalance(users)); // 20916
