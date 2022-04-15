d545358d8b395574adda11cf3399c0a5edfb69c0
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-15 11:27:56 +0300

    8

--- js/task-8.js
+++ js/task-8.js
@@ -1,1 +1,17 @@
 import users from "./users.js";
+
+const getUsersWithFriend = (users, friendName) => {
+  // твой код
+  return users.reduce((acc, user) => {
+  
+    if (user.friends.some(friend => friend === friendName)) {
+      acc.push(user.name); 
+    }
+
+  return acc;
+   }, []);
+
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
